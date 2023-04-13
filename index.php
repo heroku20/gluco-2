7cbf08ad50fb3f90e9083db90f59aa9e65fd9f79
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-04-13 18:52:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,269 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>GLUCO TRUST | REVIEW</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./img/favicon.png">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="wrp-gtrst3232">
+        <div class="wrp-gtrst32">
+            <div class="top-ban-gtrst32-rgt ">
+                <div class="top-ban-gtrst32-left tx-cr">
+                    <h2>GLUCOTRUST</h2>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--------main-ban-gtrst32---->
+    <div class="main-ban-gtrst32">
+        <div class="wrp-gtrst32">
+            <div class="main-ban-gtrst32-start fx jt-1 at-tr" style="gap:20px">
+                <div class="main-ban-gtrst32-rgt">
+                    <div class="trts-gtrst32">
+                        <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/bncw1.png" class="main-b-rsp"></a>
+                    </div>
+                </div>
+                <div class="main-ban-gtrst32-left">
+                    <div class="bnnr-ttle-gtrst32">
+
+                        <p class="trts-gtrst32">GlucoTrust is a stunning dietary supplement that supports regulating the blood sugar level with the help of the ultimate nutrition content present in the ingredients.</p>
+                        <p>1. Promote healthy blood flow and circulation.</p>
+                        <p>2. Reduce sugar and junk food cravings.</p>
+                        <p>3. Support deep, rejuvenating sleep.</p>
+                    </div>
+
+                    <div style="margin:30px 10px;">
+                        <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"
+                            class="gtrst32-ct">HURRY LIMITED TIME OFFER</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!----------------------
+<div class="ofr-sec">
+    <div class="wrp-gtrst32">
+	   <div class="off-sec-man fx jt-1 at-tr">
+	       <div class="offr-col-gtrst32">
+		      <img src="./img/brt1.jpg" class="resp21">
+		   </div>
+		   <div class="offr-col-gtrst32">
+		        <img src="./img/bnrt2.jpg" class="resp21">
+		   </div>
+	   </div>
+	</div>
+</div>
+<!---------->
+    <div class="abt" style="margin-bottom:50px;">
+        <div class="wrp-gtrst32">
+            <div class="" style="gap:20px;">
+                <div class="wht-lft">
+                    <h2 style="text-align:center;">What Is GlucoTrust?</h2>
+
+                    <p>GlucoTrust’s organic and plant-extracted formula can help the body maintain regular blood sugar levels. The product’s all-natural ingredients can regulate blood flow and balance the irregularities in your body. As a result, GlucoTrust
+                        can lessen cravings for sweets and may help to regulate your blood sugar levels.</p>
+                    <div style="margin:30px 10px;text-align:center">
+                        <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"
+                            class="gtrst32-ct">HURRY LIMITED TIME OFFER</a>
+                    </div>
+
+                    <div class="fx jt-1 at-tr" style="gap:20px;margin-top:20px; margin-bottom:20px;">
+                        <div class="wcgs-tt">
+                            <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"> <img src="./img/glucbnc1.png" class="tt-gtrst32"></a>
+                        </div>
+                        <div class="wcgs-tt">
+                            <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"> <img src="./img/gluc2e.png" class="tt-gtrst32"></a>
+                        </div>
+                        <div class="wcgs-tt">
+                            <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/glucr5.png" class="tt-gtrst32"></a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="wrp-gtrst32">
+                <div class="wht-rght">
+                    <h2 style="text-align:center"> Why Choose GlucoTrust™ Supplement</h2>
+
+                    <div class="fx jt-1 at-tr" style="gap:20px;margin-top:20px;">
+                        <div class="wcgs">
+                            <h2>Made In USA</h2>
+                            <p>Their Glucotrust dietary supplement is proudly formulated in the United States of America.</p>
+                        </div>
+                        <div class="wcgs">
+                            <h2>FDA Approved</h2>
+                            <p>Glucotrust is formulated in a FDA registered facility which adheres to strict FDA regulations.</p>
+                        </div>
+                        <div class="wcgs">
+                            <h2>GMP Certified</h2>
+                            <p>Good Manufacturing Practice certified ensuring pharmaceutical grade quality.</p>
+                        </div>
+                    </div>
+                    <div style="margin:30px 10px; text-align:center">
+                        <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"
+                            class="gtrst32-ct">HURRY LIMITED TIME OFFER</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="abt-ingt" style="margin-bottom:50px; background-color: #efeffe;">
+        <div class="wrp-gtrst32">
+            <h2 class="tx-cr">What Sorts Of Ingredients Does It Contain?</h2>
+            <p class="tx-cr">The GlucoTrust supplement is a product created from only the highest quality natural ingredients that help control blood sugar levels. </p>
+        </div>
+        <div class="wrp-gtrst32">
+            <div class="off-sec-man fx jt-1 at-tr" style="margin-top:100px">
+                <div class="offr-col-gtrst32 tx-cr t-whit">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/ingte1.png" class="resp21"></a>
+                    <h2>Gymnema Sylvestre </h2>
+                    <p> It is a herbal extract of a leafy vine. This natural ingredient has been in use for over a century for ayurvedic medicinal purposes. Gymnema promotes healthy blood sugar and curbs junk food cravings. As a result, many diabetic patients
+                        take or blood sugar support supplements that contain Gymnema Sylvestre.</p>
+                </div>
+                <div class="offr-col-gtrst32 tx-cr t-whit mrtsss">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/integ2.png" class="resp21"></a>
+                    <h2>Biotin</h2>
+                    <p>Biotin converts food into energy while metabolizing fats, carbs, and proteins. This helps in insulin production and thus ensures healthy blood sugar level. Regular intake can affect skin, hair, and nails along with contributing to
+                        healthy eyesight, liver, and nervous system.</p>
+                </div>
+            </div>
+            <div class="off-sec-man fx jt-1 at-tr" style="margin-top:100px">
+                <div class="offr-col-gtrst32 tx-cr t-whit mrtsss">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"> <img src="./img/intge3.png" class="resp21"></a>
+                    <h2>Chromium </h2>
+                    <p>Patients with Type-2 Diabetes are chromium deficient, which is associated with low blood sugar levels. GlucoTrust contains chromium to help support metabolism and fat burning.</p>
+                </div>
+                <div class="offr-col-gtrst32 tx-cr t-whit mrtsss">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"> <img src="./img/intge4.png" class="resp21"></a>
+                    <h2>Manganese </h2>
+                    <p>Promotes insulin production, so that it can convert blood sugar into energy. Manganese, according to research, promotes healthy brain and nervous system function.</p>
+                </div>
+            </div>
+            <div class="off-sec-man fx jt-1 at-tr" style="margin-top:100px">
+                <div class="offr-col-gtrst32 tx-cr t-whit mrtsss">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/intge5.png" class="resp21"></a>
+                    <h2>Cinnamon</h2>
+                    <p>It will promote healthy blood sugar and aid digestion by providing antiviral, antibacterial, and anti-inflammatory properties.</p>
+                </div>
+                <div class="offr-col-gtrst32 tx-cr t-whit mrtsss">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"> <img src="./img/intge6.png" class="resp21"></a>
+                    <h2>Licorice </h2>
+                    <p>Used mainly as weight loss aid , appetite suppressant, improving overall blood sugar, and general health and wellne-gtrst32ss support ingredient.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="abt" style="margin-bottom:50px;">
+        <div class="wrp-gtrst32">
+            <div class="fx jt-1 at-tr" style="gap:20px;">
+                <div class="prt-1">
+                    <h2>GlucoTrust Side Effects</h2>
+                    <p>GlucoTrust is an all natural supplement and there have been no reported side effects.</p>
+
+                    <p>That said, it’s recommended that you should consult with a physician or other medical professional before you start any new diet, supplement or exercise regime.</p>
+                </div>
+                <div class="prt-1">
+                    <h2>How Should I Use GlucoTrust?</h2>
+                    <p>One supplement bottle brings 30 capsules. These are enough to last you for a month as you’re supposed to take one capsules per day. All you have to do is take GlucoTrust as you have been guided on the label of the product. You can
+                        take the pills with water. Please be regular in your use of the supplement because only then can you experience effectiveness.</p>
+                </div>
+            </div>
+            <div style="margin:30px 10px;text-align:center">
+                <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"
+                    class="gtrst32-ct">HURRY LIMITED TIME OFFER</a>
+            </div>
+        </div>
+    </div>
+    <!--------->
+    <div class="glu-ofr">
+        <div class="wrp-gtrst32">
+            <h2 class="tx-cr">GlucoTrust Bonuses</h2>
+
+            <div class="fx jt-1 at-tr" style="margin-top:30px;">
+                <div class="ofr-stl-gtrst32 tx-cr">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/offer1.jpg" class="res43"></a>
+                    <h3>Digital Bonus #1 is "100 Great Tasting, Fat Burning Green Smoothie Recipes:</h3>
+                    <p>Here you\'ll discover how to whip up mouth-watering, nutrition-packed smoothies. You get a whopping 100 smoothie recipes using ingredients you can find at your local grocery store.*This image represents a digital download and not a
+                        physical book. </p>
+                </div>
+                <div class="ofr-stl-gtrst32 tx-cr">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/offer21.jpg" class="res43"></a>
+                    <h3>Digital Bonus #1 is "The Ultimate Guide To Superfoods."</h3>
+                    <p>Processed foods can lead to toxic buildup and weight gain, while superfoods support your body in every way. This report reveals common foods and snacks that have terrific health benefits. *This image represents a digital download and
+                        not a physical book. </p>
+                </div>
+                <div class="ofr-stl-gtrst32 tx-cr">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/offer23.jpg" class="res43"></a>
+                    <h3>Digital Bonus #1 is "The 3-Day Liver Cleanse Breakthrough"</h3>
+                    <p>Your liver acts a filter which absorbs and processes the food you take in. You may have toxins built up in this organ that have been affecting your energy, mood, immune system, and efforts to lose weight. digital download and not a
+                        physical book. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tx-cr" style="margin:30px 10px;">
+        <div style="margin:30px 10px;">
+            <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"
+                class="gtrst32-ct">HURRY LIMITED TIME OFFER</a>
+        </div>
+    </div>
+    <div class="mnk">
+        <div class="wrp-gtrst32">
+            <div class=" fx jt-1 at-tr gpd tx-cr">
+                <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"> <img src="./img/180-day-guarantee.png" class="res32"></a>
+                <p><b>180-DAYS 100% MONEY BACK GUARANTEE</b><br>GlucoTrust comes with a 100% money back guarantee – 180 full days from your original purchase. If you’re not totally and completely satisfied with our product or your results within the first
+                    180 days simply let us know by calling our toll-free number or dropping us an email and we’ll gladly give you a full refund within 48 hours of the product being returned.</p>
+
+            </div>
+        </div>
+    </div>
+    <div class="prc-hds" style="margin-top:30px;">
+        <div class="wrp-gtrst32">
+            <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f">
+                <h2 class="tx-cr"><u>Limited Time Special Pricing - Act Now!</u></h2>
+                <p class="tx-cr" style="font-size:20px; color:#ff0000"><u>Secure Your Reserved GlucoTrust™ While Stocks Last</u></p>
+            </a>
+        </div>
+    </div>
+    <div class="prc">
+        <div class="wrp-gtrst32">
+            <div class="fx jt-1 at-tr" style="margin-top:30px;">
+                <div class="ofr-stl-gtrst32 tx-cr">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/prc-glu1.jpg" class="res45"></a>
+                </div>
+                <div class="ofr-stl-gtrst32 tx-cr">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/prc-glu2.jpg" class="res45"></a>
+                </div>
+                <div class="ofr-stl-gtrst32 tx-cr">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"><img src="./img/prc-gl3.jpg" class="res45"></a>
+                </div>
+            </div>
+            <div class="tx-cr" style="margin:30px 10px;">
+                <div style="margin:30px 10px;">
+                    <a href="https://8mekb.bemobtrcks.com/go/569a1d91-b0ab-4ebb-96dd-ee2bd685748f"
+                        class="gtrst32-ct">HURRY LIMITED TIME OFFER</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="foter" style="margin-top:30px;">
+        <div class="wrp-gtrst32 tx-cr">
+            <small class="">CopyRights © 2023 | All Rights Reserved by Glucotrust-try.online</small>
+            <p style="margin-bottom:0px;"><a href="https://getglucotrust.com/privacy">Privacy Policy</a> | <a href="https://getglucotrust.com/terms">Terms Conditions</a> 
+        </div>
+    </div>
+
+</body>
+
+</html>
+';
+?>
